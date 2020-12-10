d03ada854b4414167fb1b2b304d6d0edfbad3c99
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-12-09 23:36:56 -0500

    Avance Curso
    Avance Curso

---
+++ Desarrollo Backend con Javascript/Curso de Closures y Scope en JavaScript/src/scope/global.js
@@ -0,0 +1,20 @@
+var hello = 'HELLO';
+
+let world = 'Hello World';
+
+const helloWorld = 'Hello World';
+
+const anotherFunction = () => {
+	console.log(hello);
+	console.log(world);
+	console.log(helloWorld);
+};
+
+anotherFunction();
+
+const helloWorld = () => {
+	globalVar = 'im global';
+};
+
+helloWorld();
+console.log(globalVar);
