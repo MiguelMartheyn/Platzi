19dec3f8c445fc2da85dcf5e1b9a338a3c54aa35
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2021-01-05 09:20:22 -0500

    Curso POO con Java SE
    Avance curso

---
+++ Curso basico Java SE/MyMedicasApointment/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Creando objeto
+        Doctor myDoctor = new Doctor("Miguel Martheyn"); //new Doctor() Metodo Constructor
+        //Instanciando objeto
+        myDoctor.name = "Alejandro Rodriguez";
+        myDoctor.showName();
+    }
+}
