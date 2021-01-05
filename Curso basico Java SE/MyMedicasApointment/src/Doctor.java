5179087598dcece7a8eb97a595001ec91b3cb011
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2021-01-05 15:28:49 -0500

    Curso POO en Java
    Avance Curso

--- Curso basico Java SE/MyMedicasApointment/src/Doctor.java
+++ Curso basico Java SE/MyMedicasApointment/src/Doctor.java
@@ -1,19 +1,73 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 //Identidad
 public class Doctor {
     //Atributos
-    int id;
-    String name;
-    String speciality;
+    int id = 0;//Auto Increment con metodo static
+    static int contador = 0;//Auto Increment con metodo static
+    private String name;
+    private String speciality;
+    private String email;
 
-    Doctor(){
-        System.out.println("Construyendo el Objeto Doctor");
-    }
-    Doctor(String name){
-        System.out.println("El nombre del Doctor asignado es: "+name);
-    }
-    //Comportamientos
 
-    public void showName(){
-        System.out.println(name);
+
+    Doctor(String name, String speciality){
+        this.name = name;
+        this.speciality = speciality;
+        contador++;
+        id = contador;
+
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+
+        availableAppointments.add(new Doctor.AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    /* Clase Aninada static. Se crea en Doctor ya que este
+    es el unico que generara citas, al ser el unico que comparte
+    informacion con esta se procede con la creacion de clase anidada.
+     */
+
+    public static class AvailableAppointment{
+        //Available Appointment
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId_availableAppointment() {
+            return id_availableAppointment;
+        }
+
+        public void setId_availableAppointment(int id_availableAppointment) {
+            this.id_availableAppointment = id_availableAppointment;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- Curso basico Java SE/MyMedicasApointment/src/Main.java
+++ Curso basico Java SE/MyMedicasApointment/src/Main.java
@@ -1,10 +1,45 @@
+import java.util.Date;
+
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        /*
         //Creando objeto
-        Doctor myDoctor = new Doctor("Miguel Martheyn"); //new Doctor() Metodo Constructor
+        Doctor myDoctor = new Doctor(); //new Doctor() Metodo Constructor
         //Instanciando objeto
         myDoctor.name = "Alejandro Rodriguez";
-        myDoctor.showName();
+        //myDoctor.showName();
+       // myDoctor.showId();
+
+        //Creando objeto
+        Doctor myDoctorAnn = new Doctor(); //new Doctor() Metodo Constructor
+        //Instanciando objeto
+        myDoctorAnn.name = "Andres Perez";
+       // myDoctorAnn.showName(); de la instancia creada trae el metodo showName
+       // myDoctorAnn.showId(); de la instancia creada trae el metodo showId
+
+        //Creando objeto
+        Patient myPatient = new Patient("Miguel Martheyn","miguel.martheyn@globant.com");
+         */
+
+        Doctor myDoctor = new Doctor("Anahi Salgado", "Pediatria");
+        myDoctor.addAvailableAppointment(new Date(),"4pm");
+        myDoctor.addAvailableAppointment(new Date(),"10am");
+        myDoctor.addAvailableAppointment(new Date(),"1pm");
+
+        for (Doctor.AvailableAppointment aA:myDoctor.getAvailableAppointments()
+             ) {
+            System.out.println(aA.getDate() + " "+ aA.getTime());
+        }
+
+        Patient patient = new Patient("Alejandra","alejandra@gmail.com");
+
+       // showMenu(); // Entra a la clase ui.UIMenu y me trae el metodo showMenu()
     }
+
+
 }
+
